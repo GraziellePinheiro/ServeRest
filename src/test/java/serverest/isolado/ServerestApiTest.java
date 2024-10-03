1572fbd556255969ae0e2bd23b36636b9aef616f
Grazielle Pinheiro <nielly_163@hotmail.com>
2024-10-02 21:54:27 -0300

    Incluindo novos testes

--- src/test/java/serverest/isolado/ServerestApiTest.java
+++ src/test/java/serverest/isolado/ServerestApiTest.java
@@ -1,4 +1,5 @@
 package serverest.isolado;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
@@ -17,6 +18,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
@@ -26,157 +28,225 @@ import io.restassured.response.Response;
 import serverest.pojo.LoginPojo;
 import serverest.pojo.UsuarioPojo;
 
-
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ServerestApiTest {
-    
+
     private static String tokenAdm;
+    private static String idUm;
+
     private UsuarioPojo usuario;
     private LoginPojo loginUsuAdm;
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         baseURI = "https://serverest.dev";
-        // port = 
+        // port =
         // basePath = "/v2";
 
-        
-        
         // this.loginAdministrador = LoginDataFactory.fazerLoginComAdm();
         // this.loginComum = LoginDataFactory.fazerLoginComUsuarioComum();
-        
+
     }
 
-    @Disabled
+    @Order(1)
     @Test
     @DisplayName("Cadastrar um usuario administrador")
-    public void testCadastrarAdmComSucesso(){
-        
-        this.usuario = new UsuarioPojo("user107", "user107@email.com.br", "user105", "true");
+    public void testCadastrarAdmComSucesso() {
 
-        String message = given()
-            .contentType(ContentType.JSON)
-            .body(usuario)
-        .when()
-            .post("/usuarios")
-        .then()
-            .log().all()
-            .statusCode(201)
-            .extract()
-            .path("message");
+        this.usuario = new UsuarioPojo("user1128", "user1128@email.com.br", "user1128", "true");
 
-        assertEquals("Cadastro realizado com sucesso", message);
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .body(usuario)
+                .when()
+                .post("/usuarios")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .extract()
+                .response();
+
+        idUm = response.path("_id");
+        System.out.println("ID capturado: " + idUm);
     }
 
-    
+    @Order(2)
     @Test
     @DisplayName("Fazer login e obter sucesso com usuario administrar")
-    public void testLoginComAdm(){
+    public void testLoginComAdm() {
 
-        this.loginUsuAdm = new LoginPojo("user107@email.com.br", "user105");
+        this.loginUsuAdm = new LoginPojo("user1128@email.com.br", "user1128");
 
-        tokenAdm = given()
-            .contentType(ContentType.JSON)
-            .body(loginUsuAdm)
-        .when()
-            .post("/login")
-        .then()
-            .log().all()
-            .statusCode(200)
-            .extract()
-            .path("authorization");
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .body(loginUsuAdm)
+                .when()
+                .post("/login")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        tokenAdm = response.path("authorization");
+
         assertNotNull(tokenAdm, "O token não deveria ser nulo");
-        assertFalse(tokenAdm.isEmpty(), "O token não deveria ser vazio");
+        assertTrue(!tokenAdm.isEmpty(), "O token não deveria ser vazio");
+
     }
+
+    @Order(3)
     @Test
     @DisplayName("Fazer login sem sucesso com usuario administrar")
-    public void testLoginComAdmSemSucesso(){
+    public void testLoginComAdmSemSucesso() {
 
         try {
-            this.loginUsuAdm = new LoginPojo("user107@email.com", "user105");
+            this.loginUsuAdm = new LoginPojo("user109@email.com", "user105");
 
-         String message = given()
-            .contentType(ContentType.JSON)
-            .body(loginUsuAdm)
-         .when()
-            .post("/login")
-         .then()
-            .log().all()
-            .statusCode(401)
-            .extract()
-            .path("message");
+            String message = given()
+                    .contentType(ContentType.JSON)
+                    .body(loginUsuAdm)
+                    .when()
+                    .post("/login")
+                    .then()
+                    .log().all()
+                    .statusCode(401)
+                    .extract()
+                    .path("message");
 
-         assertEquals("Email e/ou senha inválidos", message);
-        
+            assertEquals("Email e/ou senha inválidos", message);
+
         } catch (Exception e) {
             fail("Falha ao fazer login: " + e.getMessage());
         }
     }
 
-
+    @Order(4)
     @Test
     @DisplayName("Deve retornar uma lista de usuarios previamente cadastrados")
-    public void testDeveRetornarListaUsuarios(){
+    public void testDeveRetornarListaUsuarios() {
 
         Response response = given()
-            .queryParam("nome", "user107")
-            .queryParam("administrador", "true")
-        .when()
-            .get("/usuarios")
-        .then()
-            .log().all()
-            .statusCode(200)
-            .extract()
-            .response();
+                .queryParam("nome", "user1128")
+                .queryParam("administrador", "true")
+                .when()
+                .get("/usuarios")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .response();
 
         String responseBody = response.getBody().asString();
-        
+
         assertNotNull(responseBody, "A resposta não deveria ser nula");
 
     }
 
+    @Order(5)
     @Test
     @DisplayName("Deve retornar uma lista de usuarios previamente cadastrados sem parametros")
-    public void testDeveRetornarListaUsuariosSemEnvioDeParametros(){
+    public void testDeveRetornarListaUsuariosSemEnvioDeParametros() {
 
         Response response = when()
-            .get("/usuarios")
-        .then()
-            .log().all()
-            .statusCode(200)
-            .extract()
-            .response();
+                .get("/usuarios")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .response();
 
         String responseBody = response.getBody().asString();
-        
+
         assertNotNull(responseBody, "A resposta não deveria ser nula");
     }
 
+    @Disabled
+    @Order(6)
     @Test
     @DisplayName("Deve retornar um usuario previamente cadastrado pelo ID")
-    public void testDeveRetornarUmUsuarioPorID(){
+    public void testDeveRetornarUmUsuarioPorID() {
 
-        String usuarioID = "N2PobnQXQZJEKBbG";
+        Response response = given()
+                .pathParam("_id", idUm)
+                .when()
+                .get("/usuarios/{_id}")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .response();
 
-        try {
-            Response response = given()
-            .pathParam("_id", usuarioID)        
-         .when()
-            
-            .get("/usuarios/{_id}")
-         .then()
-            .log().all()
-            .statusCode(200)
-            .extract()
-            .response();
+        String responseBody = response.getBody().asString();
 
-         String responseBody = response.getBody().asString();
-        
-         assertNotNull(responseBody, "A resposta não deveria ser nula");
-        } catch (Exception e) {
-            fail("Erro ao buscar usuário: " + e.getMessage());
-        }
+        assertNotNull(responseBody, "A resposta não deveria ser nula");
     }
 
+    @Disabled
+    @Order(7)
+    @Test
+    @DisplayName("Deve realizar a alteração de um usuario previamente cadastrado")
+    public void testAlterarUmUsuarioCadastrado() {
+
+        UsuarioPojo usuarioEditado = new UsuarioPojo("user1126", "user1126@email.com.br", "user1126", "true");
+
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .pathParam("_id", idUm)
+                .body(usuarioEditado)
+                .when()
+                .put("/usuarios/{_id}")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        String responseBody = response.getBody().asString();
+
+        assertNotNull(responseBody, "A resposta não deveria ser nula.");
+
+    }
+
+    @Order(8)
+    @Test
+    @DisplayName("Deve retornar erro ao tentar editar usuário que não existe")
+    public void testDeveRetornarErroAoEditarUsuarioInexistente() {
+        String usuarioIDInexistente = "inexistenteID"; // ID de um usuário que não existe
+
+        UsuarioPojo usuarioEditado = new UsuarioPojo("inexistente", "inexistente@qa.com.br", "fbatista", "false");
+
+        given()
+                .contentType(ContentType.JSON)
+                .pathParam("_id", usuarioIDInexistente)
+                .body(usuarioEditado)
+                .when()
+                .put("/usuarios/{_id}")
+                .then()
+                .log().all()
+                .statusCode(400)
+                .body("message", equalTo("Este email já está sendo usado"));
+    }
+
+    @Order(9)
+    @Test
+    @DisplayName("Deve excluir um usuário existente")
+    public void testDeveExcluirUsuarioExistente() {
+
+        Response response = given()
+                .pathParam("_id", idUm)
+                .when()
+                .delete("/usuarios/{_id}")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        String responseBody = response.getBody().asString();
+        String expectedMessage = "Registro excluído com sucesso";
+
+        assertTrue(responseBody.contains(expectedMessage));
+    }
 
 }
